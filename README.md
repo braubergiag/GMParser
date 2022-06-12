40d751fec3cf3fd2e49a11ab849a76ab4bb61f1e
glushkov <glushkov@protei.ru>
2022-06-12 23:39:43 +0300

    read.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# GMParser
+# Simple gmi parser
+# cd GMParser
+# cmake .
+# cmake -S . -B build
+# cmake --build build
+# cd build && ctest
+# ./GMParser input_src target_src
--- src/main.cc
+++ src/main.cc
@@ -3,7 +3,6 @@
 #include <string>
 
 
-
 int main(int argc, char *argv[]) {
     #if 1
     if (argc != 3) {
@@ -14,7 +13,7 @@ int main(int argc, char *argv[]) {
     #endif
     SiteGenerator sitegen(argv[1],argv[2]);
     try {
-         sitegen.Generate();
+        sitegen.Generate();
     } catch (std::runtime_error & ex) {
         std::cerr << ex.what() << std::endl;
     } 
